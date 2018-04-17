63ed612f5df864c1c8a7e19d0ccb0f6070d5cb60
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-17 19:46:52 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,5 +3,7 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  
+  def artist_select(artist, song)
+    
+  end 
 end
