86fd4be5f534fc55a9fb61977273c0580d70579d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-17 19:52:06 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,9 +5,9 @@ module ArtistsHelper
 
   def artist_select(artist, song)
     if song.artist.nil?
-      select_tag "artist_id", options_from_collection_for_select(Artist.all, :id, :name)
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
-      hidden_field_tag "artist_id", song.artist_id
+      hidden_field_tag "song[artist_id]", song.artist_id
     end
   end
 end
