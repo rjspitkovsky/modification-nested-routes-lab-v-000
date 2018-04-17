18371226c41c3db77ec1a0164eb59e1145399262
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-17 19:43:52 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
 
   def new
     if params[:artist_id] && !Artist.exists?(params[:artist_id])
-      redirect_to artists_path, alert: "Artist not found"
+      redirect_to artists_path, alert: "Artist not found."
     else 
       @song = Song.new(artist_id: params[:artist_id])
     end
@@ -43,8 +43,18 @@ class SongsController < ApplicationController
   end
 
   def edit
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path, alert: "Author not found."
+      else
+        @song = artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(artist), alert: "Song not found." if @song.nil?
+      end 
+    else 
     @song = Song.find(params[:id])
-  end
+    end
+  end 
 
   def update
     @song = Song.find(params[:id])
